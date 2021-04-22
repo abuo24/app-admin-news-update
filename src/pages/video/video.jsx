1674c66bcc536875e2c27c7e9ba86ba19838b1f5
Orifjon Yunusjonov <mryediniofficial9924@gmail.com>
2021-04-22 18:20:49 +0500

    finally change

--- src/pages/video/video.jsx
+++ src/pages/video/video.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Button, Col, Form, Input, message, Pagination, Row, Select, Upload} from "antd";
+import {Button, Col, Form, Input, message, Row, Select, Upload} from "antd";
 import {postsApi} from "../../redux/service/postsApi";
 import {toast, ToastContainer} from "react-toastify";
 import {shortNewsApi} from "../../redux/service/shortNewsApi";
@@ -19,41 +19,16 @@ const Video = (props) => {
         titleRu: "",
         link: ""
     });
-    const [postList, setPostList] = useState(null);
-
-    const [paginationList, setPaginationList] = useState({
-        totalPage: 0,
-        totalItems: 0,
-        current: 1,
-        minIndex: 0,
-        maxIndex: 0,
-        pageSize: 0
-    });
 
     useEffect(() => {
-        props.getVideos().then(res => {
-            setPostList({posts: res.payload.data.videos})
-            setPaginationList({
-                ...paginationList,
-                current: res.payload.data.currentPage + 1,
-                minIndex: 0,
-                totalItems: res.payload.data.totalItems,
-                maxIndex: res.payload.data.videos.length,
-                pageSize: res.payload.data.videos.length,
-                totalPage: res.payload.data.totalPages
-            });
-
-        }).catch(err => console.log(err))
-
-    }, []);
-
-
-    // const [videos, setVideos] = useState(props.videos_reducer && props.videos_reducer.all && props.videos_reducer.all.data)
+        props.getVideos()
+    }, [])
+    const [videos, setVideos] = useState(props.videos_reducer && props.videos_reducer.all && props.videos_reducer.all.data)
 
     useEffect(() => {
-        // setPostList({...postList,posts:props.videos_reducer && props.videos_reducer.all && props.videos_reducer.all.data && props.videos_reducer.all.data.videos});
+        setVideos(props.videos_reducer && props.videos_reducer.all && props.videos_reducer.all.data)
     })
-    const getVideos = postList && postList.posts&& postList.posts.map((item, key) => (
+    const getVideos = Array.isArray(videos) && videos.map((item, key) => (
         <LatestVideoItem key={key} title={item.titleUz} link={item.link} id={item.id}/>
     ));
 
@@ -63,43 +38,22 @@ const Video = (props) => {
 
     const onFinish = () => {
         videosApi.add(dataPost).then(res => {
-            props.getVideos().then(res => {
-                setPostList({posts: res.payload.data.videos})
-                setPaginationList({
-                    ...paginationList,
-                    // current: res.payload.data.currentPage + 1,
-                    minIndex: 0,
-                    totalItems: res.payload.data.totalItems,
-                    maxIndex: res.payload.data.videos.length,
-                    pageSize: res.payload.data.videos.length,
-                    totalPage: res.payload.data.totalPages
-                });
-                handleChange(paginationList.current)
-            }).catch(err => console.log(err))
-
+            props.getVideos()
+            console.log(res);
             note();
             form.resetFields();
         }).catch(err => {
-             danger()
+            console.log(err);
+            danger()
         })
     };
     const [form] = Form.useForm();
 
     useEffect(() => {
-        setPostList({posts:props.videos_reducer && props.videos_reducer.all && props.videos_reducer.all.data && props.videos_reducer.all.data.videos});
+        // console.log(props.category_reducer.categories)
     }, [props]);
 
-    const handleChange = (page) => {
-        props.getVideos(page - 1).then(res => {
-            setPostList({posts: res.payload.data.videos})
-            setPaginationList({
-                ...paginationList,
-                current: page,
-                minIndex: 0,
-                maxIndex: res.payload.data.videos.length,
-            });
-        }).catch(err => console.log(err))
-    };
+    console.log(dataPost);
 
     return (
             <div className="App">
@@ -191,13 +145,6 @@ const Video = (props) => {
                 <Row>
                     {getVideos}
                 </Row>
-                {paginationList.totalPage > 0 ? <Pagination
-                    pageSize={paginationList.pageSize && paginationList.pageSize}
-                    current={paginationList.current && paginationList.current}
-                    total={paginationList.totalItems && paginationList.totalItems}
-                    onChange={handleChange}
-                    style={{marginTop: "10px"}}
-                /> : ""}
             </div>
     );
 };
